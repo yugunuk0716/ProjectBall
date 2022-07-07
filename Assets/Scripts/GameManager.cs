e17821f97a263dc7ccf409ac8af8a6084070cd5a
김성후 <68197093+kim-Eukkbi@users.noreply.github.com>
2022-07-07 20:13:00 +0900

    대충 타이머 만들어 봤습니다

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -19,18 +20,31 @@ public class GameManager : MonoBehaviour
     public float limitTime = 1f;
     public float firstTime = 0f;
 
+    public TextMeshProUGUI timer_text;
+
+    private float realTime;
+    private IEnumerator timerCo;
+
     private void Awake()
     {
         instance = this;
+        realTime = 0;
     }
 
-
+    public void Start()
+    {
+        timerCo = Timer();
+    }
 
     public void CheckClear()
     {
-        if(firstTime == 0f)
+        
+        if (firstTime == 0f)
         {
             firstTime = Time.time;
+            StartCoroutine(timerCo);
+            timer_text.text = "Ready";
+            timer_text.color = Color.white;
         }
 
         List<Goal> list = goalList.FindAll(goal => !goal.isChecked);
@@ -40,17 +54,34 @@ public class GameManager : MonoBehaviour
             print("클리어");
             print(Time.time - firstTime);
             firstTime = 0f;
+            realTime = 0f;
+            StopCoroutine(timerCo);
+            timer_text.text = "Clear";
+            timer_text.color = Color.green;
         }
         else if(list.Count <= 0)
         {
             print("실패");
-            print(Time.time - firstTime );
+            print(Time.time - firstTime);
             foreach (Goal goal in goalList)
             {
                 goal.ResetFlag();
             }
             firstTime = 0f;
+            realTime = 0f;
+            StopCoroutine(timerCo);
+            timer_text.text = "Reset";
+            timer_text.color = Color.red;
+        }
+    }
 
+    public IEnumerator Timer()
+    {
+        while(true)
+        {
+            yield return null;
+            realTime += Time.deltaTime;
+            timer_text.text = string.Format("{0:0.00}", limitTime - realTime <= 0 ? "0:00" : limitTime - realTime);
         }
     }
 }
