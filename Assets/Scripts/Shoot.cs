f082e804df710e2436bd8e9f6c13c69cab4ca6ed
DESKTOP-MG9TML9\USER <qheorjsdnr@naver.com>
2022-07-07 16:14:11 +0900

    발수 제한

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -7,6 +7,9 @@ public class Shoot : MonoBehaviour
     public Shooter shooter;
     public Shooter curShooter;
 
+    public const int ammoCount = 10;
+    public int curAmmoCount = 0;
+
     private void Start()
     {
         SpawnShooter();
@@ -28,9 +31,11 @@ public class Shoot : MonoBehaviour
 
     public void SpawnShooter()
     {
+        if (ammoCount <= curAmmoCount)
+            return;
         Shooter _shooter = Instantiate(shooter);
 
-      
+        curAmmoCount++;
 
         _shooter.transform.position = this.transform.position;
         curShooter = _shooter;
