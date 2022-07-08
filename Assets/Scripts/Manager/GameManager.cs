77f2fe3e77d5ec9215266b631055c47efdd24d91
DESKTOP-MG9TML9\USER <qheorjsdnr@naver.com>
2022-07-08 15:11:41 +0900

    스테이지 파싱

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -18,7 +18,7 @@ public class GameManager : MonoBehaviour
 
     public List<Goal> goalList = new List<Goal>();
 
-    public float limitTime = 1f;
+    public float limitTime = 2f;
     public float firstTime = 0f;
 
     public TextMeshProUGUI timer_text;
@@ -60,20 +60,7 @@ public class GameManager : MonoBehaviour
             timer_text.text = "Clear";
             timer_text.color = Color.green;
         }
-        else if(list.Count <= 0)
-        {
-            print("실패");
-            print(Time.time - firstTime);
-            foreach (Goal goal in goalList)
-            {
-                goal.ResetFlag();
-            }
-            firstTime = 0f;
-            realTime = 0f;
-            StopCoroutine(timerCo);
-            timer_text.text = "Reset";
-            timer_text.color = Color.red;
-        }
+       
     }
 
     public IEnumerator Timer()
@@ -82,6 +69,23 @@ public class GameManager : MonoBehaviour
         {
             yield return null;
             realTime += Time.deltaTime;
+            if(limitTime - realTime <= 0)
+            {
+                print("실패");
+                print(Time.time - firstTime);
+                foreach (Goal goal in goalList)
+                {
+                    goal.ResetFlag();
+                }
+                firstTime = 0f;
+                realTime = 0f;
+                StopCoroutine(timerCo);
+                timer_text.text = "Reset";
+                timer_text.color = Color.red;
+                yield return new WaitForSeconds(0.2f);
+                timer_text.color = Color.white;
+                timer_text.text = "Ready";
+            }
             timer_text.text = string.Format("{0:0.00}", limitTime - realTime <= 0 ? "0:00" : limitTime - realTime);
         }
     }
