67a5cb16feac2e7c226490370567efa102bfefc4
DESKTOP-MG9TML9\USER <qheorjsdnr@naver.com>
2022-07-29 21:30:29 +0900

    스테이지 스크립터블 오브젝트 만들었어요

---
+++ Assets/Scripts/Objects/Stage/StageDataSO.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "SO/StageDataSO")]
+public class StageDataSO : ScriptableObject
+{
+    public int ballCount;
+    public Ball[] balls;
+    public float countDown;
+
+    public string jsonString;
+}
