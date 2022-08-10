fc601e831393cacf8cd90e60d58c47173096466e
DESKTOP-RO87EIT\user <juhyeong0823@gmail.com>
2022-08-10 19:09:42 +0900

    스프라이트 렌더러 제거 및 UI 변경중

---
+++ Assets/Scripts/UI/BallControllUI.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallControllUI : UIBase
+{
+    public Image img;
+    public Image directionImg;
+    public Image stateImg;
+    public override void Init()
+    {
+        // 할 거 없음
+    }
+}
