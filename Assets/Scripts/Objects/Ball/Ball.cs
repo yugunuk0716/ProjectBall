df8bc8cd09b771a20f23877a5a42a63e2f986153
yugunuk0716 <qheorjsdnr@naver.com>
2022-09-01 14:56:49 +0900

    * Ball.cs: 공에 색 추가
    * CamTest.cs: 상수들 변수로 바꿈

--- Assets/Scripts/Objects/Ball/Ball.cs
+++ Assets/Scripts/Objects/Ball/Ball.cs
@@ -48,6 +48,8 @@ public class Ball : PoolableMono
     public Vector2 myPos;
     public float speed = 0.25f;
 
+    public Color currentColor = Color.white;
+
     private void Awake()
     {
         anim = GetComponentInChildren<Animator>();
@@ -150,5 +152,9 @@ public class Ball : PoolableMono
         this.GetComponentInChildren<SpriteRenderer>().color = Color.red;
     }    
 
-   
+   public void ColorChange(Color newColor)
+    {
+        currentColor = newColor;
+        sr.color = currentColor;
+    }
 }
