ac360766a873b7924a81180f2cbc663ab8e424d7
juhyeong0823 <juhyeong0823@gmail.com>
2022-09-13 12:48:33 +0900

    인스펙터 빈거 추가, 인스펙터 수정

--- Assets/Scripts/UI/SettingPopUp.cs
+++ Assets/Scripts/UI/SettingPopUp.cs
@@ -7,19 +7,19 @@ public class SettingPopUp : UIBase
 {
     public Button menuButton;
     public Button resumeButton;
-    public Button optionButton;
+    //public Button optionButton;
     public Button homeButton;
     public Button quitButton;
 
-    public Button sfxButton;
-    public Button bgmButton;
+    //public Button sfxButton;
+    //public Button bgmButton;
 
     public override void Init()
     {
         GetCanvasGroup();
         menuButton.onClick.AddListener(() => ScreenOn(true));
         resumeButton.onClick.AddListener(() => ScreenOn(false));
-        optionButton.onClick.AddListener(() => { }); // 옵션 버튼에 옵션 패널 띄우게 만들기
+        //optionButton.onClick.AddListener(() => { }); // 옵션 버튼에 옵션 패널 띄우게 만들기
         homeButton.onClick.AddListener(() => { }); // 버튼에 씬이동하게 만들기
         quitButton.onClick.AddListener(() => Application.Quit());
     }
