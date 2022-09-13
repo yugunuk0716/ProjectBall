a9fe7c88d4ea162c9f3a6b8901dc4b8ca9896a11
yugunuk0716 <qheorjsdnr@naver.com>
2022-09-13 12:43:31 +0900

    UI 수정 및 대포 만드는 중

--- Assets/Scripts/UI/SettingPopUp.cs
+++ Assets/Scripts/UI/SettingPopUp.cs
@@ -19,7 +19,6 @@ public class SettingPopUp : UIBase
         GetCanvasGroup();
         menuButton.onClick.AddListener(() => ScreenOn(true));
         resumeButton.onClick.AddListener(() => ScreenOn(false));
-        optionButton.onClick.AddListener(() => { }); // 옵션 버튼에 옵션 패널 띄우게 만들기
         homeButton.onClick.AddListener(() => { }); // 버튼에 씬이동하게 만들기
         quitButton.onClick.AddListener(() => Application.Quit());
     }
