26577c1072c8076cc16183306e6b453130b592b9
yugunuk0716 <qheorjsdnr@naver.com>
2022-09-15 15:52:55 +0900

    스테이지 드래그

---
+++ Assets/Scripts/UI/StageScrollUI.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using AirFishLab.ScrollingList.Demo;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StageScrollUI : UIBase
+{
+    public Button stageOnBtn;
+    public Button[] stageButtons;
+    public int[] btnInteractCount;
+
+    public override void Init()
+    {
+        GetCanvasGroup();
+        stageButtons = GetComponentsInChildren<Button>();
+        btnInteractCount = new int[stageButtons.Length];
+        StageManager sm = IsometricManager.Instance.GetManager<StageManager>();
+
+        stageOnBtn.onClick.AddListener(() => { ScreenOn(true); });
+
+        for (int i = 0; i < stageButtons.Length; i++)
+        {
+            int temp = i;
+            stageButtons[temp].onClick.AddListener(() =>
+            {
+
+                //if(btnInteractCount[temp] < 1)
+                //{
+                //    btnInteractCount[temp] += 1;
+                //    return;
+                //}
+
+                //btnInteractCount[temp] = 0;
+                print(int.Parse(stageButtons[temp].GetComponent<IntListBox>()._contentText.text));
+                sm.LoadStage(Resources.Load<StageDataSO>($"Stage {int.Parse(stageButtons[temp].GetComponent<IntListBox>()._contentText.text) + 1}"));
+                ScreenOn(false);
+            });
+        }
+    }
+
+    public override void Load()
+    {
+        
+    }
+
+}
