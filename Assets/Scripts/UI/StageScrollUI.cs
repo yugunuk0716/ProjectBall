46c883e53e2ec0dd79879297fe2bd19ff7da2863
yugunuk0716 <qheorjsdnr@naver.com>
2022-09-19 18:32:18 +0900

    유아이수정중

--- Assets/Scripts/UI/StageScrollUI.cs
+++ Assets/Scripts/UI/StageScrollUI.cs
@@ -11,28 +11,51 @@ public class StageScrollUI : UIBase
     public Button[] stageButtons;
 
     private bool isScreenOn = false;
-
+    bool canEnter = false;
+    
+    int index;
+    StageManager sm;
     public List<IntListBox> allContents = new List<IntListBox>();
 
     public override void Init()
     {
         GetCanvasGroup();
-        StageManager sm = IsometricManager.Instance.GetManager<StageManager>();
+        sm = IsometricManager.Instance.GetManager<StageManager>();
         stageButtons = GetComponentsInChildren<Button>();
         allContents = GetComponentsInChildren<IntListBox>().ToList();
         IsometricManager.Instance.GetManager<GameManager>().UpdateUIContents += () => { allContents.ForEach(c => c.UpdateContent()); };
 
-        stageOnBtn.onClick.AddListener(() => { ScreenOn(!isScreenOn); isScreenOn = !isScreenOn; });
+        stageOnBtn.onClick.AddListener(() =>
+        {
+            ScreenOn(!isScreenOn);
+            isScreenOn = !isScreenOn;
+            ASD();
+        });
 
+       
+    }
+
+    public void ASD()
+    {
         for (int i = 0; i < stageButtons.Length; i++)
         {
             int temp = i;
+            IntListBox box = stageButtons[temp].GetComponentInParent<IntListBox>();
+            index = 0;
+            bool a = int.TryParse(box._contentText.text, out index);
+            print(box._contentText.text);
+            bool canEnter = false;
+            if (sm.clearMapCount + 1 >= index)
+            {
+                canEnter = true;
+
+            }
+            box.SetLock(!canEnter);
             stageButtons[temp].onClick.AddListener(() =>
             {
                 print($"temp {temp}, length {stageButtons.Length}");
-                int index = int.Parse(stageButtons[temp].GetComponentInParent<IntListBox>()._contentText.text);
                 print($"idx:{index}, cc: {sm.clearMapCount}");
-                if (sm.clearMapCount + 1 >= index)
+                if (canEnter)
                 {
                     sm.LoadStage(index);
                     ScreenOn(false);
