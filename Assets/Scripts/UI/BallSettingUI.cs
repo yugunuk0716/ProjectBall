b5494d3a553a7fa17a7303c827c082d265dafe11
juhyeong0823 <juhyeong0823@gmail.com>
2022-09-20 00:20:52 +0900

    UI 문제 해결, 이제 UI 풀링하고 오류 체크, 공 출입 연출 등 할 듯?

--- Assets/Scripts/UI/BallSettingUI.cs
+++ Assets/Scripts/UI/BallSettingUI.cs
@@ -1,6 +1,9 @@
+using DG.Tweening;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BallSettingUI : UIBase
 {
@@ -16,12 +19,25 @@ public class BallSettingUI : UIBase
     [SerializeField] BallControllUI ballControllUIPrefab;
     [SerializeField] GameObject targetPointObjPrefab;
 
+    [Header("Panel")]
     [SerializeField] private SelectDirectionUI selectDirectionUI;
+    [SerializeField] private RectTransform shootPanel; // 공 발사할 때 볼 패널!
+
+    [HideInInspector] public RectTransform setOnBtn;
+    [HideInInspector] public RectTransform shootOnBtn;
+
+    [Header("Button")]
+    public Button confirmBtn;
+    [SerializeField] Button shootBtn;
+
+    public Action SwitchUI; // 세팅에서 슛으로.
+    public Action RollbackShootUI; // 변화했던 UI 돌려놓기.
+
+    [SerializeField] RectTransform targetPoint_ShootBtn;
 
     public override void Init()
     {
         GameManager gm = IsometricManager.Instance.GetManager<GameManager>();
-
         gm.MakeNewBallUI = (ball, isAutoSet) =>
         {
             BallControllUI newBallControllUI = Instantiate(ballControllUIPrefab, ballContent);
@@ -38,6 +54,8 @@ public class BallSettingUI : UIBase
                 gm.myBallList.Add(ball);
                 newBallControllUI.SetDirection(ball.shootDir);
             }
+
+            
 
             newBallControllUI.directionSetBtn.onClick.AddListener(() =>
             {
@@ -62,26 +80,74 @@ public class BallSettingUI : UIBase
                 isAdded = !isAdded;
             });
         };
+
+        confirmBtn.onClick.AddListener(() =>
+        {
+            if (false == GameManager.CanNotInteract && gm.myBallList.Count >= gm.maxBallCount)
+            {
+                gm.ballUIList.ForEach((x) => x.directionSetBtn.interactable = false);
+                SwitchUI();
+                StartCoroutine(MoveBallUis(gm.ballUIList));
+            }
+        });
+
+        shootBtn.onClick.AddListener(() => gm.Shoot());
     }
 
     public override void Load()
     {
         order = 0;
-
         Transform []arr = targetPointContent.GetComponentsInChildren<Transform>();
-
         for(int i = 1; i < arr.Length; i++)
         {
             Destroy(arr[i].gameObject);
         }
-        MakeTargetPoints();
 
+        MakeTargetPoints();
+        RollbackShootUI?.Invoke();
+    }
+
+    IEnumerator MoveBallUis(List<BallControllUI> list)
+    {
+        yield return new WaitForSeconds(2f);
+
+        GameManager.CanNotInteract = true;
+        Transform[] targetPoints = targetPointContent.GetComponentsInChildren<Transform>(); // 걍 0번은 무시하고 가죠
+
+        float duration = 0.3f;
+        float minusDuration = duration / targetPoints.Length / 2;
+
+        yield return null;
+
+        for (int i = 0; i < list.Count; i++)
+        {
+            list[i].transform.SetParent(targetPoints[i + 1]);
+            list[i].transform.DOMove(targetPoints[i + 1].position, 1f).SetEase(Ease.OutCubic);
+            yield return new WaitForSeconds(duration);
+            duration -= minusDuration;
+        }
+
+        yield return new WaitForSeconds(0.3f);
+
+        Sequence rollbackUISeq = DOTween.Sequence();
+        rollbackUISeq.SetAutoKill(false);
+        rollbackUISeq.Append(shootOnBtn.DOAnchorPosX(300, 0.6f).SetEase(Ease.InQuart));
+        rollbackUISeq.Join(setOnBtn.DOAnchorPosX(500, 0.5f).SetEase(Ease.InQuart).SetDelay(0.2f));
+
+        rollbackUISeq.Append(shootPanel.DOAnchorPosY(-50, 0.6f).SetEase(Ease.InQuart));
+
+        rollbackUISeq.Append(shootBtn.GetComponent<RectTransform>().DOAnchorPos(targetPoint_ShootBtn.anchoredPosition, 0.7f).SetEase(Ease.OutCubic));
+        rollbackUISeq.Join(shootBtn.transform.DORotate(new Vector3(0, 0, 360), 1f, RotateMode.LocalAxisAdd));
+        rollbackUISeq.Join(shootBtn.transform.DOScale(new Vector3(2, 2, 2), 1f).OnComplete(() => GameManager.CanNotInteract = false));
+
+        RollbackShootUI = () => rollbackUISeq.PlayBackwards();
+        
     }
 
     public void MakeTargetPoints()
     {
         StageManager sm = IsometricManager.Instance.GetManager<StageManager>();
-        int count = sm.stageDataList[sm.stageIndex].balls.Length;
+        int count = sm.stageDataList[sm.stageIndex - 1].balls.Length;
 
         for (int i = 0; i < count; i++)
         {
--- Assets/Scripts/UI/IngameUI.cs
+++ Assets/Scripts/UI/IngameUI.cs
@@ -12,28 +12,7 @@ public class IngameUI : UIBase
     public TextMeshProUGUI debugText;
     public TextMeshProUGUI timer_text;
 
-    /*
-    IEnumerator MoveBalls(List<BallControllUI> list)
-    {
-        Transform[] targetPoints = targetPointContent.GetComponentsInChildren<Transform>(); // 걍 0번은 무시하고 가죠
 
-        float duration = 0.3f;
-        float minusDuration = duration / targetPoints.Length / 2 ;
-
-
-        for (int i = 0; i< list.Count; i++)
-        {
-            list[i].transform.SetParent(targetPoints[i + 1]);
-            list[i].transform.DOMove(targetPoints[i + 1].position, 1f).SetEase(Ease.OutCubic);
-            yield return new WaitForSeconds(duration);
-            duration -= minusDuration;
-        }
-
-        //이거 나중에 고치삼 ㅇㅋ? ㅇㅋ?ㅋㅇ?ㅋㅇㅋ?ㅇㅋ?ㅇㅋ?ㅇㅋ?ㅇ
-        yield return new WaitForSeconds(1f);
-        GameManager.CanNotInteract = false;
-    }
-    */
 
     public override void Init()
     {
