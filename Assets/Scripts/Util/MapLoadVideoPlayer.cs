e3009c7ad0d54e7b9b12eff56c2d8adcef51a263
DESKTOP-P118A3I\Eukkbi <smbaekr56@naver.com>
2022-09-19 20:25:24 +0900

    시발 주형

--- Assets/Scripts/Util/MapLoadVideoPlayer.cs
+++ Assets/Scripts/Util/MapLoadVideoPlayer.cs
@@ -4,7 +4,7 @@ using UnityEngine.UI;
 
 public class MapLoadVideoPlayer : MonoBehaviour
 {
-    public Texture2D[] texes = new Texture2D[200];
+    public Texture2D[] texes = new Texture2D[120];
     // 나중에 미리 생성해두고 그걸 초기화하는 느낌으로 ㄱㄱ
 
     public RawImage img;
@@ -12,7 +12,7 @@ public class MapLoadVideoPlayer : MonoBehaviour
 
     private void Awake()
     {
-        for (int i = 0; i < 200; i++)
+        for (int i = 0; i < 120; i++)
         {
             texes[i] = new Texture2D(rt.width, rt.height); // 전체적으로 생성
         }
@@ -36,7 +36,7 @@ public class MapLoadVideoPlayer : MonoBehaviour
             RenderTexture.active = rt;
             texes[i].ReadPixels(new Rect(0, 0, rt.width, rt.height), 0, 0);
             texes[i].Apply();
-            yield return new WaitForSeconds(0.02f);
+            yield return new WaitForSeconds(0.03f);
         }
 
         Debug.Log("완료");
@@ -47,7 +47,7 @@ public class MapLoadVideoPlayer : MonoBehaviour
         for (int i = 0; i < texes.Length; i++)
         {
             img.texture = texes[i];
-            yield return new WaitForSeconds(0.02f);
+            yield return new WaitForSeconds(0.03f);
         }
     }
 }
