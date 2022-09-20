6a9cd33ef0f25d6da8ed15014f0944a6d6d26d23
DESKTOP-P118A3I\Eukkbi <smbaekr56@naver.com>
2022-09-20 19:21:09 +0900

    구름 완성

--- Assets/Scripts/Objects/CloudHandler.cs
+++ Assets/Scripts/Objects/CloudHandler.cs
@@ -7,24 +7,45 @@ public class CloudHandler : MonoBehaviour
 {
     private List<Cloud> clouds = new List<Cloud>();
     
-    public void CloudMove()
+    public IEnumerator CloudMove()
     {
-        for (int i = 0; i < 7; i++)
+        for (int i = 0; i < 12; i++)
         {
             Cloud cloud = PoolManager.Instance.Pop("Cloud") as Cloud;
             ResetCloud(cloud);
             clouds.Add(cloud);
+            yield return new WaitForSeconds(Random.Range(.5f, 3f));
         }
     }
 
     public void ResetCloud(Cloud cloud)
     {
-        cloud.transform.position = new Vector3(10f, Random.Range(-12f, 10f), 0);
+        cloud.transform.position = new Vector3(10f, Random.Range(-12f, 8), 0);
         cloud.gameObject.SetActive(true);
         SpriteRenderer sr = cloud.GetComponent<SpriteRenderer>();
-        sr.DOFade(Random.Range(.5f, 1), .5f);
 
-        cloud.transform.DOMove(new Vector3(-10, cloud.transform.position.y + 5), Random.Range(3f,10f)).SetEase(Ease.Linear).OnComplete(() =>
+        float sacle = Random.Range(.5f, 1.5f);
+        float alpha;
+        float speed;
+
+        cloud.transform.localScale = new Vector3(sacle, sacle, 0);
+
+        if (sacle <= .7f)
+        {
+            alpha = Random.Range(.7f, 1f);
+            sr.sortingOrder = -(int)(100 * alpha);
+            speed = Random.Range(10f, 15f);
+        }
+        else
+        {
+            alpha = Random.Range(.3f, 7f);
+            sr.sortingOrder = (int)(10 + alpha);
+            speed = Random.Range(5f, 10f);
+        }
+
+        sr.DOFade(alpha, .5f);
+
+        cloud.transform.DOMove(new Vector3(-10, cloud.transform.position.y + 5), speed).SetEase(Ease.Linear).OnComplete(() =>
         {
             ResetCloud(cloud);
         });
