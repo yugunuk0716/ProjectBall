7eb7d8dc3c8246ccf75d5326b46bb6dae94245fe
DESKTOP-P118A3I\Eukkbi <smbaekr56@naver.com>
2022-09-27 19:39:30 +0900

    아오ㅓ시발 튜토리얼 25%

--- Assets/Scripts/Manager/TutorialManager.cs
+++ Assets/Scripts/Manager/TutorialManager.cs
@@ -10,9 +10,11 @@ public class TutorialManager : ManagerBase
 {
     private List<CanvasGroup> tutoPanels = new List<CanvasGroup>();
     private TuroritalUI turoritalUI;
+    private RectTransform arrowText;
     private GameManager gm;
     private UIManager um;
     private BallControllUI currentBallUI;
+    private Canvas currentSelectedCanvas;
     private int ballCount = 0;
     //ManagerBase 구현하기
     public override void Init()
@@ -25,10 +27,15 @@ public class TutorialManager : ManagerBase
     {
         um = IsometricManager.Instance.GetManager<UIManager>();
         gm = IsometricManager.Instance.GetManager<GameManager>();
+        arrowText = GameObject.Find("Arrow").GetComponent<RectTransform>();
+
 
         turoritalUI = um.canvas[3].GetComponent<TuroritalUI>();
 
         turoritalUI.TutoPanels.ForEach(x => tutoPanels.Add(x.GetComponent<CanvasGroup>()));
+
+        tutoPanels.ForEach(x => x.alpha = 0);
+
         ballCount = 0;
     }
 
@@ -46,27 +53,63 @@ public class TutorialManager : ManagerBase
         um.canvas[3].blocksRaycasts = true;
         um.canvas[3].DOFade(1f, 1f);
         
+        
+        
         yield return null;
         SelectBall();
     }
 
     public void SelectBall()
     {
-        currentBallUI = gm.ballUIList[ballCount];
-        Canvas c;
-        currentBallUI.gameObject.AddComponent<GraphicRaycaster>();
-        currentBallUI.gameObject.AddComponent<Canvas>();
-        c = currentBallUI.gameObject.GetComponent<Canvas>();
-        c.overrideSorting = true;
-        c.sortingOrder = 210;
-        currentBallUI.directionSetBtn.onClick.AddListener(ChooseDir);
+        tutoPanels.ForEach(x => x.alpha = 0);
+        tutoPanels[0].alpha = 1;
+
+        if (gm.ballUIList.Count <= ballCount)
+        {
+            Confirm();
+        }
+        else
+        {
+            arrowText.offsetMax = new Vector2(arrowText.offsetMax.x + 380 * ballCount, arrowText.offsetMax.y);
+            currentBallUI = gm.ballUIList[ballCount];
+
+            currentBallUI.gameObject.AddComponent<GraphicRaycaster>();
+            currentBallUI.gameObject.AddComponent<Canvas>();
+            currentSelectedCanvas = currentBallUI.gameObject.GetComponent<Canvas>();
+            currentSelectedCanvas.overrideSorting = true;
+            currentSelectedCanvas.sortingOrder = 210;
+            currentBallUI.directionSetBtn.onClick.AddListener(ChooseDir);
+        }
     }
 
     public void ChooseDir()
     {
+        tutoPanels.ForEach(x => x.alpha = 0);
+        tutoPanels[1].alpha = 1;
+        
         Destroy(currentBallUI.GetComponent<GraphicRaycaster>());
         Destroy(currentBallUI.GetComponent<Canvas>());
+
+
+        SelectDirectionUI selectDirectionUI = currentBallUI.transform.GetComponentInParent<BallSettingUI>().selectDirectionUI;
+        selectDirectionUI.gameObject.AddComponent<GraphicRaycaster>();
+        selectDirectionUI.gameObject.AddComponent<Canvas>();
+        currentSelectedCanvas = selectDirectionUI.gameObject.GetComponent<Canvas>();
+        currentSelectedCanvas.overrideSorting = true;
+        currentSelectedCanvas.sortingOrder = 210;
+
+        ballCount++;
+        selectDirectionUI.selectDirectionBtns.ForEach(x => x.onClick.AddListener(SelectBall));
+
         tutoPanels[0].DOFade(0, 1f);
         tutoPanels[1].DOFade(1, 1f);
+    }
+
+    public void Confirm()
+    {
+        tutoPanels.ForEach(x => x.alpha = 0);
+        tutoPanels[2].alpha = 1;
+
+        
     }
 }
