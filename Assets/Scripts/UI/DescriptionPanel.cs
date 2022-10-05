9d92fad6168e51ff8beac150e4be77e7584d0e9b
yugunuk0716 <qheorjsdnr@naver.com>
2022-10-05 09:58:02 +0900

    설명 패널 연결

---
+++ Assets/Scripts/UI/DescriptionPanel.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DescriptionPanel : UIBase
+{
+    public Button descButton;
+    public Button closeButton;
+
+    public override void Init()
+    {
+        GetCanvasGroup();
+
+        descButton.onClick.AddListener(() =>
+        {
+            ScreenOn(true);
+            print("on");
+        });
+
+        closeButton.onClick.AddListener(() =>
+        {
+            ScreenOn(false);
+            print("off");
+        });
+    }
+
+    public override void Load()
+    {
+
+    }
+
+    public override void Reset()
+    {
+
+    }
+
+}
